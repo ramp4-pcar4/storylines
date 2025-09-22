7d54f6512298d073415dbf48f960858f70807f4e
Yi Lei Feng <fengyilei1@gmail.com>
2025-09-22 18:32:46 +0000

    Delete PR demo from GH pages gallery-tweaks

--- gallery-tweaks/assets/panel-xrBnR7ci.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./story-CVEP8yjd.js";import"./main-CreAmVPr.js";export{m as default};
--- gallery-tweaks/assets/useT9n-ao51JRwP-Bb9pDRec.js
+++
@@ -1,1 +0,0 @@
-import{i7 as o}from"./story-CVEP8yjd.js";import{u as r}from"./jsxFactory-C5LxVioS-BwFoEjo5.js";const s=o(r);export{s};
