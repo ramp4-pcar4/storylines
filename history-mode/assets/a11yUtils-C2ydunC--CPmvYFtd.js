2a0ee854a04a6a323f57a90b06d6cbf1d145b1f0
Yi Lei Feng <fengyilei1@gmail.com>
2025-10-24 15:20:16 +0000

    Delete PR demo from GH pages history-mode

--- history-mode/assets/a11yUtils-C2ydunC--CPmvYFtd.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./story-BQe_Usfr.js";const o=()=>e.respectPrefersReducedMotion&&window.matchMedia("(prefers-reduced-motion: reduce)").matches;export{o as r};
--- history-mode/assets/vue.esm-bundler-EeyxqZGJ-x9qTCzrA.js
+++
@@ -1,1 +0,0 @@
-import{gQ as o,hN as s}from"./story-BQe_Usfr.js";const r=o(s);export{r as s};
