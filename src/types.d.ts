d6d372c13a993ba7c3b3dcb699d53badc706112b
Ryan Coulson <ryan.coulson@ec.gc.ca>
2025-12-02 14:25:21 -0500

    add help modal

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,12 @@
 import { VuePapaParse } from 'vue-papa-parse';
 import { RouteLocationNormalized } from 'vue-router';
+import type { Vfm } from 'vue-final-modal';
 
 declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
         $papa: VuePapaParse;
         $route: RouteLocationNormalized;
         $t: (key: string, ...args: any[]) => string;
+        $vfm: Vfm;
     }
 }
