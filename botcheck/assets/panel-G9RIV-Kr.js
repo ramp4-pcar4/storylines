e9b01976be8d07d246539f5e08d60f35f8efd08f
Yi Lei Feng <fengyilei1@gmail.com>
2026-03-30 15:23:29 +0000

    Delete PR demo from GH pages botcheck

--- botcheck/assets/panel-G9RIV-Kr.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./story-FtgNxjzO.js";import"./main-CXw6KN7R.js";export{m as default};
--- botcheck/assets/useT9n-ao51JRwP-Du0qW7PL.js
+++
@@ -1,1 +0,0 @@
-import{i7 as o}from"./story-FtgNxjzO.js";import{u as r}from"./jsxFactory-C5LxVioS-D07qpYBe.js";const s=o(r);export{s};
